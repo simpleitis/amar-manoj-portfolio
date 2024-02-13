6cdc3cd3815e87f6c8f6d9340f0b8778ffd4511d
Amar Krishna <amarkrishna25@gmail.com>
2024-02-13 18:52:12 +0530

    fix ui

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -4,10 +4,19 @@ const Contact = () => {
   return (
     <div
       id="contact"
-      className="p-10 text-4xl font-bold flex flex-col gap-10 rounded-xl hover:scale-110 duration-300 border"
+      className="p-10 text-2xl font-bold flex flex-col rounded-xl hover:scale-110 duration-300 border mx-10 w-[30%] mb-10 isolate aspect-video bg-white/20 shadow-lg ring-1 ring-black/5"
     >
-      <p>Email: amarm1234@gmail.com</p>
-      <p>Phone: 9567977334</p>
+      <p className="text-xl md:text-5xl dark:text-white font-bold mb-10 underline">
+        Contact
+      </p>
+      <div>
+        <p className="text-4xl text-lime-400">Amar Manoj</p>
+        <p className="text-2xl">Automobile Engineer</p>
+      </div>
+      <div className="flex flex-col justify-start items-end">
+        <p className="text-xl text-neutral-300">amarm1234@gmail.com</p>
+        <p className="text-xl text-neutral-300">9567977334</p>
+      </div>
     </div>
   )
 }
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className="w-screen bg-neutral-800 flex justify-center mt-20 p-5 gap-2">
+    <div className="w-screen bg-neutral-800 flex justify-center p-5 gap-2">
       <p>© All rights reserved by Amar Manoj</p>
       <Image src="/piston.png" alt="footer" width={30} height={30} />
     </div>
