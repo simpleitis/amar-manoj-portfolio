061603ac4d2a8d094d8a524bf94b2c88a65ff34a
Amar Krishna <amarkrishna25@gmail.com>
2024-02-13 19:28:07 +0530

    fix UI issue of contact

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -4,7 +4,7 @@ const Contact = () => {
   return (
     <div
       id="contact"
-      className="p-10 text-2xl font-bold flex flex-col rounded-xl hover:scale-110 duration-300 border mx-10 w-[30%] mb-10 isolate aspect-video bg-white/20 shadow-lg ring-1 ring-black/5"
+      className="p-10 text-2xl font-bold flex flex-col rounded-xl hover:scale-110 duration-300 border w-[90%] sm:w-[60%] lg:w-[50%] xl:w-[40%] mb-10"
     >
       <p className="text-xl md:text-5xl dark:text-white font-bold mb-10 underline">
         Contact
@@ -13,7 +13,7 @@ const Contact = () => {
         <p className="text-4xl text-lime-400">Amar Manoj</p>
         <p className="text-2xl">Automobile Engineer</p>
       </div>
-      <div className="flex flex-col justify-start items-end">
+      <div className="flex flex-col justify-start sm:items-end mt-5 ">
         <p className="text-xl text-neutral-300">amarm1234@gmail.com</p>
         <p className="text-xl text-neutral-300">9567977334</p>
       </div>
